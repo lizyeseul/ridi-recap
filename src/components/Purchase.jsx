e5d17ae811f832ae3388e3c9b7a4ee66bba59c18
lizyeseul <lys984prms@naver.com>
2026-03-05 22:57:26 +0900

    샘플 데이터 추가 등

--- src/components/Purchase.jsx
+++ src/components/Purchase.jsx
@@ -1,4 +1,4 @@
-import UTIL from "@/scripts/utils.js"
+import UTIL from "@/scripts/utils.js";
 
 import { useState } from "react";
 
@@ -6,58 +6,80 @@ import DB from "@/scripts/connect_db.js";
 import SYNC_PURCHASE from "@/scripts/sync/sync_purchase.js";
 
 function Purchase() {
-	const [tempData, setTempData] = useState();
+  const [tempData, setTempData] = useState();
 
-	const [selectedStore, setSelectedStore] = useState();
-	// const [searchKey, setSearchKey] = useState();
-	const [searchValue, setSearchValue] = useState();
-	const [searchLimit, setSearchLimit] = useState();
-	async function findData() {
-		var r;
-		let searchKey = "order_no";
-		if(selectedStore == "unit") searchKey = "unit_id";
-		if(selectedStore == "book") searchKey = "book_id";
-		if(UTIL.isNotEmpty(searchValue)) {
-			r = await DB.getUniqueValue("store_"+selectedStore, searchKey, UTIL.toNumber(searchValue));
-		}
-		else {
-			r = await DB.getValueByIdx("store_"+selectedStore, searchKey, { limit: searchLimit });
-		}
-    	console.log(r)
-		setTempData(JSON.stringify(r));
-	}
-	async function syncPurchase() {
-		SYNC_PURCHASE.syncPurchase();
-	}
-	return (
-		<div>
-			<div>
-				<button onClick={findData}>데이터 조회</button>
-				<button onClick={() => {setTempData()}}>데이터 초기화</button>
-				<br/>
-				<select name="store" onChange={(e) => {setSelectedStore(e.target.value)}}>
-					<option value="order">order</option>
-					<option value="unit">unit</option>
-					<option value="book">book</option>
-				</select>
-				{/* <input	type="text"	name="storeKey"
+  const [selectedStore, setSelectedStore] = useState();
+  // const [searchKey, setSearchKey] = useState();
+  const [searchValue, setSearchValue] = useState();
+  const [searchLimit, setSearchLimit] = useState();
+  async function findData() {
+    var r;
+    let searchKey = "order_no";
+    if (selectedStore == "unit") searchKey = "unit_id";
+    if (selectedStore == "book") searchKey = "book_id";
+    if (UTIL.isNotEmpty(searchValue)) {
+      r = await DB.getUniqueValue(
+        "store_" + selectedStore,
+        searchKey,
+        selectedStore == "order" ? searchValue : UTIL.toNumber(searchValue),
+      );
+    } else {
+      r = await DB.getValueByIdx("store_" + selectedStore, searchKey, {
+        limit: searchLimit,
+      });
+    }
+    setTempData(JSON.stringify(r));
+  }
+  async function syncPurchase() {
+    SYNC_PURCHASE.syncPurchase();
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={findData}>데이터 조회</button>
+        <button
+          onClick={() => {
+            setTempData();
+          }}
+        >
+          데이터 초기화
+        </button>
+        <br />
+        <select
+          name="store"
+          onChange={(e) => {
+            setSelectedStore(e.target.value);
+          }}
+        >
+          <option value="order">order</option>
+          <option value="unit">unit</option>
+          <option value="book">book</option>
+        </select>
+        {/* <input	type="text"	name="storeKey"
 						placeholder="key"
 						onChange={(e) => setSearchKey(e.target.value)}/> */}
-				<input	type="text"	name="storeSearchValue"
-						placeholder="value"
-						onChange={(e) => setSearchValue(e.target.value)}/>
-				<input	type="number"	name="storeLimit"
-						placeholder="limit"
-						onChange={(e) => setSearchLimit(e.target.value)}/>
-			</div>
-			<hr/>
-			{tempData}
-			{UTIL.isNotEmpty(tempData) ? <hr/> : null}
-			<div>
-				<button onClick={syncPurchase}>데이터 동기화</button>
-			</div>
-		</div>
-	);
+        <input
+          type="text"
+          name="storeSearchValue"
+          placeholder="value"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <input
+          type="number"
+          name="storeLimit"
+          placeholder="limit"
+          onChange={(e) => setSearchLimit(e.target.value)}
+        />
+      </div>
+      <hr />
+      {tempData}
+      {UTIL.isNotEmpty(tempData) ? <hr /> : null}
+      <div>
+        <button onClick={syncPurchase}>데이터 동기화</button>
+      </div>
+      <hr />
+    </div>
+  );
 }
 
 export default Purchase;
--- src/scripts/sync/sync_purchase.js
+++ src/scripts/sync/sync_purchase.js
@@ -3,29 +3,48 @@ import purchaseClass from "@/scripts/schema/purchase.js";
 import UTIL from "@/scripts/utils.js";
 
 var SYNC_PURCHASE = {
-	syncPurchase: async function() {
-//		let orderList = await DB.getValueByIdx("store_order", "order_no", null);
-		let orderList = await DB.getUniqueValue("store_order", "order_no", "2024121965524800");
-		/*
+  syncPurchase: async function () {
+    console.clear();
+    //		let orderList = await DB.getValueByIdx("store_order", "order_no", null);
+    let orderNo = "2026022694763556";
+    let orderList = await DB.getUniqueValue("store_order", "order_no", orderNo);
+    /*
 		111043064 리셋레 세트
 		1508004622 세트에 연결된 book 2권?
 		2822000211 2권 구판
 		3885003834 2권 신판
 		*/
-		orderList = [orderList];
-		for(let orderData of orderList) {
-			if(orderData == null) continue;
-			let bookList = orderData.book_list;
-			for(let bookId of Object.keys(bookList)) {
-				// if(bookId != 111043064) continue;
-				let bookData = await DB.getUniqueValue("store_book", "book_id", UTIL.toNumber(bookId));
-				let unitData = await DB.getUniqueValue("store_unit", "unit_id", UTIL.toNumber(bookData.unit_id));
+    orderList = [orderList];
+    for (let orderData of orderList) {
+      console.log(`ORDER : ======= ${orderNo}`);
+      console.log(orderData);
+      if (orderData == null) continue;
+      let bookList = orderData.book_list;
+      for (let bookId of Object.keys(bookList)) {
+        // if(bookId != 111043064) continue;
+        let bookData = await DB.getUniqueValue(
+          "store_book",
+          "book_id",
+          UTIL.toNumber(bookId),
+        );
+        let unitData = await DB.getUniqueValue(
+          "store_unit",
+          "unit_id",
+          UTIL.toNumber(bookData.unit_id),
+        );
 
-				let purchaseItem = new purchaseClass();
-				purchaseItem.setRawClass(orderData, unitData, bookData);
-				console.log({...purchaseItem.order.dataForPurchase, ...purchaseItem.unit.dataForPurchase, ...purchaseItem.book.dataForPurchase});
-			}
-		}
-	}
+        let purchaseItem = new purchaseClass();
+        purchaseItem.setRawClass(orderData, unitData, bookData);
+        console.log(`${bookData.unit_id},${bookId}`);
+        console.log(unitData);
+        console.log(bookData);
+        console.log({
+          ...purchaseItem.order.dataForPurchase,
+          ...purchaseItem.unit.dataForPurchase,
+          ...purchaseItem.book.dataForPurchase,
+        });
+      }
+    }
+  },
 };
 export default SYNC_PURCHASE;
