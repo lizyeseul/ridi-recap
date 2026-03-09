a1272ed8eafdc3eb550cf23457e711338e794f83
owo <39235968+lizyeseul@users.noreply.github.com>
2026-03-09 08:10:04 +0000

    260309

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,14 +2,16 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import InitPage from "./login";
 import Home from "./components/Home";
+import DBAdmin from "./components/DBAdmin";
 
 export default function Index() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<InitPage />} />
-          <Route path="/Home/*" element={<Home />} />
+          <Route path="/" element={<DBAdmin />} />
+          {/* <Route path="/" element={<InitPage />} />
+          <Route path="/Home/*" element={<Home />} /> */}
         </Routes>
       </Router>
     </>
