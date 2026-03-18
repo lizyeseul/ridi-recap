a30b2570c3dff3fed3170c84b1637dc0dc8ae9f7
owo <39235968+lizyeseul@users.noreply.github.com>
2026-03-18 05:49:59 +0000

    260318-1

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,7 +2,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import InitPage from "./login";
 import Home from "./components/Home";
-import DBAdmin from "./components/DBAdmin";
+import DBAdmin from "./showDB/components/DBAdmin";
 
 export default function Index() {
   return (
---
+++ src/showDB/stores/useDBResultList.jsx
@@ -0,0 +1,21 @@
+import create from "zustand";
+
+const useDBResultStore = create((set) => ({
+  selectedIdx: -1,
+  setSelectedIdx: (idx) => set({ selectedIdx: idx }),
+
+  showData: {},
+  setShowData: (data) => set({ showData: data }),
+
+  resultList: [],
+  addResultData: (data) =>
+    set((prev) => ({
+      resultList: [...prev.resultList, data],
+    })),
+  removeResultData: (idx) =>
+    set((prev) => ({
+      resultList: prev.splice(idx),
+    })),
+}));
+
+export default useDBResultStore;
